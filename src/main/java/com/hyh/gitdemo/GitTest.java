8ec10772d6988933e4ba730f343d201638834aca
黄耀华 <781931990@qq.com>
2021-06-02 21:26:14 +0800

    hot-fix commit2

--- src/main/java/com/hyh/gitdemo/GitTest.java
+++ src/main/java/com/hyh/gitdemo/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("hello, test2");
         System.out.println("hello, test3");
         System.out.println("hello, test4");
+        System.out.println("hot-fix, test");
     }
 }
