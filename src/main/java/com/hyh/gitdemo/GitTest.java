7b4c0a4337b174e148a00b299f2f70162acd27dd
黄耀华 <781931990@qq.com>
2021-06-02 17:25:50 +0800

    first commit

---
+++ src/main/java/com/hyh/gitdemo/GitTest.java
@@ -0,0 +1,15 @@
+package com.hyh.gitdemo;
+
+/**
+ * @ClassName GitTest
+ * @Description
+ * @Author huangyaohua
+ * @Date 2021-06-02 17:23
+ * @Version
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello, test");
+    }
+}
